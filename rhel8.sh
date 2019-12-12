b5b5bb18ef0bfd7538615c09cea8c135c6602a3a
Roland Hansmann <han@ti8m.ch>
2019-12-12 15:23:03 +0100

    :pencil: add net.ipv4.icmp_echo_bold_ignore_broadcasts

--- rhel8.sh
+++ rhel8.sh
@@ -559,6 +559,7 @@ net.ipv4.conf.default.log_martians=1
 net.ipv4.route.flush=1
 net.ipv4.icmp_echo_ignore_broadcasts=1
 net.ipv4.icmp_ignore_bogus_error_responses=1
+net.ipv4.icmp_echo_bold_ignore_broadcasts=1
 net.ipv4.conf.all.rp_filter=1
 net.ipv4.conf.default.rp_filter=1
 net.ipv4.tcp_syncookies=1
